5af8784ad12d5a73caeeef6b687f1da7a62feee8
Yana Chachovskaya <a9506862137@yandex.ru>
2022-10-12 12:18:48 +0300

    methods

--- main.js
+++ main.js
@@ -31,24 +31,27 @@
 
 let obj = {here: {is: "an"}, object: 2};
 
-function deepEqual(a, b){
-  if (a === b) return true;
-  if (a == null || typeof a != "object" ||
-      b == null || typeof b != "object") return false;
+// function deepEqual(a, b){
+//   if (a === b) return true;
+//   if (a == null || typeof a != "object" ||
+//       b == null || typeof b != "object") return false;
  
-  let keysA = Object.keys(a), keysB = Object.keys(b);
+//   let keysA = Object.keys(a), keysB = Object.keys(b);
 
-   if (keysA.length != keysB.length) return false;
-   for (let key of keysA){
-    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
-   }
-   return true;
-}
+//    if (keysA.length != keysB.length) return false;
+//    for (let key of keysA){
+//     if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+//    }
+//    return true;
+// }
 
 
 
-console.log(deepEqual(obj, obj));
+// console.log(deepEqual(obj, obj));
 
-console.log(deepEqual(obj, {here: 1, object: 2}));
+// console.log(deepEqual(obj, {here: 1, object: 2}));
 
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+
+let newMassive = Object.keys(obj);
+console.log(newMassive);
