208d8a610023dba7a92b9afdeac4cf9891323681
Yana Chachovskaya <a9506862137@yandex.ru>
2022-10-12 11:40:31 +0300

    deep comparison objects

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
-function arrayToList(array) {
-    let list = null;
-    for (let i = array.length - 1; i >= 0; i--) {
-      list = {value: array[i], rest: list};
-    }
-    return list;
-  }
-  console.log(arrayToList([1, 2, 3]));
+// function arrayToList(array) {
+//     let list = null;
+//     for (let i = array.length - 1; i >= 0; i--) {
+//       list = {value: array[i], rest: list};
+//     }
+//     return list;
+//   }
+//   console.log(arrayToList([1, 2, 3]));
 
-  function listToArray(list){
-    let array = [];
-    for (let node = list; node; node = node.rest){
-        array.push(node.value);
-    }
-    return array;
-}
-console.log(listToArray(arrayToList([1, 2, 3])));
+//   function listToArray(list){
+//     let array = [];
+//     for (let node = list; node; node = node.rest){
+//         array.push(node.value);
+//     }
+//     return array;
+// }
+// console.log(listToArray(arrayToList([1, 2, 3])));
 
-function prepend(value, list){
-    return {value, rest: list};
-}
-console.log(prepend(10, prepend(20, null)));
+// function prepend(value, list){
+//     return {value, rest: list};
+// }
+// // console.log(prepend(10, prepend(20, null)));
 
-function nth(list, n){
-    if (!list) return undefined;
-    else if(n == 0) return list.value;
-    else return nth(list.rest, n - 1);
+// function nth(list, n){
+//     if (!list) return undefined;
+//     else if(n == 0) return list.value;
+//     else return nth(list.rest, n - 1);
+// }
+// // console.log(nth(arrayToList([10, 20, 30]), 2));
+
+
+let obj = {here: {is: "an"}, object: 2};
+
+function deepEqual(a, b){
+  if (a === b) return true;
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+ 
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+   if (keysA.length != keysB.length) return false;
+   for (let key of keysA){
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+   }
+   return true;
 }
-console.log(nth(arrayToList([10, 20, 30]), 2));
+
+
+
+console.log(deepEqual(obj, obj));
+
+console.log(deepEqual(obj, {here: 1, object: 2}));
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
